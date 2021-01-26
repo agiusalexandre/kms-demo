6fcae8041949363fb9c8e5625bdb32cb53cac03e
Agius Alexandre <aaagius@amazon.fr>
2021-01-26 19:06:53 +0100

    update

--- README.md
+++ README.md
@@ -1,87 +1,30 @@
-# KMS Developer Demo
-
-## Description
-
-This is a developer oriented demonstration on how to use [AWS Key Management Service (KMS)]( https://aws.amazon.com/kms/) to encrypt plain text or files in the Python programming language, although this example and API used is also relevant for developers using other programming languages.
-
-AWS Key Management Service (KMS) is a managed service that makes it easy for you to create and control the encryption keys used to encrypt your data, and uses Hardware Security Modules (HSMs) to protect the security of your keys.
-
 ## Comments
 
-This demo is divided in four main parts.
+This demo is divided in 3 options ( client side encryption ) :
+- Option 1: Using a CMK stored in AWS KMS
+- Option 2: Using a CMK stored in AWS KMS & Using AWS KMS encrypt-decrypt operation
+- ption 3: Using AWS Encryption SDK is a client-side encryption library designed
 
-### Part 1 : Infrastructure Setup
+## Prerequisites create virtual Env
 
-This part is required to setup the infrastructure required by the demo:
-- create a role allowing you to use KMS  
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install boto3 aws-encryption-sdk pycryptodome 
 
-- create a KMS Master key if it does not exist yet.  
+## Create KMS Symmetric CMK
 
-  Each key that you create in AWS Key Management Service costs $1/month as long as it is enabled, therefore, for this demo, we choose to create a KMS Master key in your account and to not delete it at the end of the demo, instead we will de-activate it and reuse it later for the next run of the demo.  
+aws kms create-key \
+    --tags TagKey=Purpose,TagValue=Test \
+    --description "Development test key" 
 
-- create an S3 bucket (to be used in part 3 below)
-- Establish the connection to the KMS service in the given region
+## Update the CMK ARN
 
-### Part 2 : Clear Text encoding / decoding
+into main.py search for MASTER_KEY_ARN and update it.
 
-The next two code blocks are dealing with clear text encryption and decryption.
+## links
+boto3 KMS :  https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html
+cli KMS: https://docs.aws.amazon.com/cli/latest/reference/kms/index.html
+AWS encryption SDK : https://pypi.org/project/aws-encryption-sdk/
+AWS encryption SDK AWS DOc : https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/introduction.html
 
-#### Cipher
-
-The code request a data key from KMS.  KMS returns the key as cleartext and as a ciphered object.  Code is using the cleartext key to encode the text message.  
-
-In real life scenario, code should dispose the cleartext version of the key and store the ciphered key only.
-
-#### Decipher
-
-The code makes a KMS call, passing the cipher version of the key and receive the clear text key back.
-
-It then uses this clear text key to decipher the ciphered message.
-
-### Part 3 : File encoding / decoding
-
-The next two code blocks perform a cipher / decipher operation on a JPG file and upload the file to an S3 bucket.
-
-Notice the following:
-
-- The AES Initialization Vector (IV) is stored in the ciphered file header (<Q+original file length+IV)
-
-- The ciphered version of the data key is base64 encoded and stored in the S3 MetaData, as "Key"
-
-Deciphering is following similar steps as the clear text version described above.  
-
-### Part 4 : Clean up
-
-The clean part of the code takes care of deleting files and the S3 bucket.  It then disable the KMS master key.
-
-## Costs
-
-KMS cost metrics are:
-- a fixed cost per month for enabled master keys
-- a price per 10000 API calls.
-
-The free tier includes 20000 API calls per month.
-
-Details and up-to-date information is available in [KMS Pricing page](https://aws.amazon.com/kms/pricing/)
-
-S3 cost to host the 11k JPG file is negligible.
-
-## Credits
-
-File encryption code taken from http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/ published under a [Public Domain License](http://unlicense.org)
-
-## License
-
-Copyright 2015, Amazon Web Services.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--- main.py
+++ main.py
@@ -1,53 +1,91 @@
-__author__ = 'stormacq'
-
+__author__ = 'aaagius'
+import boto3
 from boto3.session import Session
 from boto3.s3.transfer import S3Transfer
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto import Random
-import random, os, struct, base64
+import random
+import os
+import struct
+import base64
 
-MASTER_KEY_ARN = 'arn:aws:kms:us-east-1:486652066693:key/44d25f19-fda9-48ed-88d8-4c8afd0e837b'
-S3_BUCKET = 'public-sst'
-DIRECTORY = '/Users/stormacq/Desktop'
-FILENAME  = 'wifi.jpg'
+MASTER_KEY_ARN = 'YOUR-ARN'
+S3_BUCKET = 'demo-cmk'
+#DIRECTORY = '/Users/aaagius/Desktop'
+#FILENAME = 'wifi.jpg'
+REGION = 'eu-west-1'
+
+
+###########################################################################
+# Option 1: Using a CMK stored in AWS KMS
+# Scenario : Encrypt / Decrypt on client side with AES lib
+#            AWS KMS is use to protect the Data key
+#            AWS s3 store the ciphered data & the ciphered key
+#####
+# Call Kms API to Generate a Data Key
+# Use the PyCryptodome AES module for AES block cipher ( https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html )
+# Create an AES key with the returned  256 bit generated KMS key
+# Encode a plain text with the cipher key
+# Decode the cipher text with the cipher key
+###########################################################################
 
 def kmsKeyDemo():
-    print('\nHello KMS Key Demo\n')
+    print('\n Option 1: Using a CMK stored in AWS KMS \n')
 
     #
     # Create a KMS Client object
     #
-    session = Session(profile_name='default', region_name='us-east-1')
+    session = Session(profile_name='default', region_name=REGION)
     kms = session.client('kms')
 
     #
     # Generate a Data Key (encoded with my Master Key in KMS)
-    #
-    key = kms.generate_data_key(KeyId=MASTER_KEY_ARN,KeySpec='AES_256')
-    keyPlain  = key['Plaintext']
+    #    Response {
+    #     'CiphertextBlob': b'bytes',
+    #     'Plaintext': b'bytes',
+    #     'KeyId': 'string'
+    #    }
+    # A plaintext version of the data key that the client uses to encrypt the object data
+    # A cipher blob of the same data key that the client uploads to Amazon S3 as object metadata
+
+    key = kms.generate_data_key(KeyId=MASTER_KEY_ARN, KeySpec='AES_256')
+
+    keyPlain = key['Plaintext']
     keyCipher = key['CiphertextBlob']
-    # print(key)
+
+    print(' Plain text generated key = %s \n ' % keyPlain)
+    print(' Ciphered text Blob = %s \n ' % keyCipher)
 
     #
     # Encode a plain text with the data key
     #
     obj = AES.new(keyPlain, AES.MODE_CBC, b'This is an IV123')
-    msgPlain  = b'Hello world of cryptography w/managed keys'
+    msgPlain = b'Hello world of cryptography w/managed keys'
     msgCipher = obj.encrypt(pad(msgPlain, 16))
-
-    print ('Plain text key = %s' % base64.b64encode(keyPlain))
-    print ('Plain text msg = %s' % msgPlain)
-    print ('Cipher message = %s' % base64.b64encode(msgCipher))
+    print('AES encrypt \n')
+    print('Plain text msg to encrypt = %s \n' % msgPlain)
+    print('Cipher message = %s \n' % base64.b64encode(msgCipher))
 
     #
     # Now, we're supposed to trash the clear text key
     # and save the cipher version of the key
     #
+    # s3_client = boto3.client('s3')
+
+    # s3_client.create_bucket(Bucket=S3_BUCKET,CreateBucketConfiguration={
+    # 'LocationConstraint': 'eu-west-1'})
+
+    # response = s3_client.upload_file(msgCipher, S3_BUCKET, 'msgCipher')
+    # response = s3_client.upload_file(keyCipher, S3_BUCKET, 'keyCipher')
 
     #
     # Later, we ask KMS to create a plain text version of the cipher key
+    # Send the cipher blob to AWS KMS to get the plaintext version of the data key
+    #   -> so we can decrypt the object data
     #
+
+    print('AES decrypt \n')
     key = kms.decrypt(CiphertextBlob=keyCipher)
     keyPlain = key['Plaintext']
 
@@ -57,35 +95,45 @@ def kmsKeyDemo():
     obj = AES.new(keyPlain, AES.MODE_CBC, b'This is an IV123')
     plainText = unpad(obj.decrypt(msgCipher), 16)
 
-    print (f'Plain text msg = {plainText}')
+    print(f'Plain text msg decrypted = {plainText} \n')
+
+
+###########################################################################
+# Option 2: Using a CMK stored in AWS KMS & Using AWS KMS encrypt-decrypt operation
+# Scenario :
+#            AWS KMS is use to protect the Data key & for encrypt-Decrypt operation
+#            AWS s3 store the ciphered data & the ciphered key
+###########################################################################
 
 def kmsEncryptionDemo():
 
-    print ('\nHello KMS Password encryption Demo\n')
+    print('\n Option 2: Using a CMK stored in AWS KMS & Using AWS KMS encrypt-decrypt operation \n')
 
     #
     # Create a KMS Client object
     #
-    session = Session(profile_name='default', region_name='us-east-1')
+    session = Session(profile_name='default', region_name=REGION)
     kms = session.client('kms')
 
     password = 'this is my super secret password'
-    print(f'Plaintext password  = {password}')
+    print(f'Plaintext password  = {password} \n')
 
     #
     # Cipher a plain text object using your master key
     #
-    ret = kms.encrypt(KeyId=MASTER_KEY_ARN,Plaintext=password)
-    print (f"Cipher password    = {base64.b64encode(ret['CiphertextBlob'])}")
+    ret = kms.encrypt(KeyId=MASTER_KEY_ARN, Plaintext=password)
+    print('AWS KMS encrypt \n')
+    print(f"Cipher password    = {base64.b64encode(ret['CiphertextBlob'])} \n")
 
     #
     # Decrypt a ciphered text
     #
     ret = kms.decrypt(CiphertextBlob=ret['CiphertextBlob'])
-    print (f"Plaintext password = {ret['Plaintext']}")
+    print('AWS KMS decrypt \n')
+    print(f"Plaintext password = {ret['Plaintext']} \n")
 
 
-#file encryption code taken from http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
+# file encryption code taken from http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
 def S3KMSDemo():
 
     #
@@ -97,26 +145,26 @@ def S3KMSDemo():
     #
     # Generate a Data Key (encoded with my Master Key in KMS)
     #
-    key = kms.generate_data_key(KeyId=MASTER_KEY_ARN,KeySpec='AES_256')
-    keyPlain  = key['Plaintext']
+    key = kms.generate_data_key(KeyId=MASTER_KEY_ARN, KeySpec='AES_256')
+    keyPlain = key['Plaintext']
     keyCipher = key['CiphertextBlob']
 
     #
     # Encode a file with the data key
     #
-    print ('Initializing encryption engine')
+    print('Initializing encryption engine')
     iv = Random.new().read(AES.block_size)
     chunksize = 64*1024
     encryptor = AES.new(keyPlain, AES.MODE_CBC, iv)
 
-    print (f'KMS Plain text key = {base64.b64encode(keyPlain)} ')
-    print (f'KMS Encrypted key  = {base64.b64encode(keyCipher)} ')
+    print(f'KMS Plain text key = {base64.b64encode(keyPlain)} ')
+    print(f'KMS Encrypted key  = {base64.b64encode(keyCipher)} ')
 
     in_filename = os.path.join(DIRECTORY, FILENAME)
     out_filename = in_filename + '.enc'
     filesize = os.path.getsize(in_filename)
 
-    print ('Encrypting file')
+    print('Encrypting file')
     with open(in_filename, 'rb') as infile:
         with open(out_filename, 'wb') as outfile:
             outfile.write(struct.pack('<Q', filesize))
@@ -133,40 +181,41 @@ def S3KMSDemo():
     # Store encrypted file on S3
     # Encrypted Key will be stored as meta data
     #
-    print ('Storing encrypted file on S3')
+    print('Storing encrypted file on S3')
     metadata = {
-        'key' : base64.b64encode(keyCipher).decode('ascii')
+        'key': base64.b64encode(keyCipher).decode('ascii')
     }
 
     s3 = session.client('s3')
-    s3.upload_file(out_filename, S3_BUCKET, out_filename, ExtraArgs={'Metadata' : metadata})
+    s3.upload_file(out_filename, S3_BUCKET, out_filename,
+                   ExtraArgs={'Metadata': metadata})
     os.remove(out_filename)
 
     ##
-    ## Later ...
+    # Later ...
     ##
 
     #
     # Download Encrypted File and it's metadata
     #
-    print ('Download file and meta data from S3')
+    print('Download file and meta data from S3')
     transfer = S3Transfer(s3)
     transfer.download_file(S3_BUCKET, out_filename, out_filename)
 
-    #retrieve meta data
+    # retrieve meta data
     import boto3
     s3 = boto3.resource('s3')
     object = s3.Object(S3_BUCKET, out_filename)
-    #print object.metadata
+    # print object.metadata
 
     keyCipher = base64.b64decode(object.metadata['key'])
 
-    #decrypt encrypted key
-    print ('Decrypt ciphered key')
+    # decrypt encrypted key
+    print('Decrypt ciphered key')
     key = kms.decrypt(CiphertextBlob=keyCipher)
     keyPlain = key['Plaintext']
-    print (f'KMS Plain text key = {base64.b64encode(keyPlain)}')
-    print (f'KMS Encrypted key  = {base64.b64encode(keyCipher)}')
+    print(f'KMS Plain text key = {base64.b64encode(keyPlain)}')
+    print(f'KMS Encrypted key  = {base64.b64encode(keyCipher)}')
 
     #
     # Decrypt the file
@@ -193,39 +242,58 @@ def S3KMSDemo():
     # Cleanup S3
     object.delete()
 
-    print (f'Done.\n\nYour file {out_filename} should be identical to original file {os.path.join(DIRECTORY, FILENAME)}')
+    print(
+        f'Done.\n\nYour file {out_filename} should be identical to original file {os.path.join(DIRECTORY, FILENAME)}')
 
-# from https://github.com/aws/aws-encryption-sdk-python
-# see http://busy-engineers-guide.reinvent-workshop.com/ 
+
+###########################################################################
+# Option 3: Using AWS Encryption SDK is a client-side encryption library designed
+# to make it easy for everyone to encrypt and decrypt data using industry standards and best practices
+# https://aws-encryption-sdk-python.readthedocs.io/en/latest/index.html
+# Scenario :
+#            AWS KMS is use to protect the Data key & for encrypt-Decrypt operation
+#            AWS s3 store the ciphered data & the ciphered key
+###########################################################################
 def encryptionSDKDemo():
     import aws_encryption_sdk
+    from aws_encryption_sdk.identifiers import CommitmentPolicy
+
 
     #
-    # AWS Encryption SDK works with arbitrary length text 
+    # Create AWS encryption sdk client with 
     #
-    plaintext = 'This is a very long text document'
+    client = aws_encryption_sdk.EncryptionSDKClient(        
+        commitment_policy=CommitmentPolicy.FORBID_ENCRYPT_ALLOW_DECRYPT
+    )
+
+    kms_key_provider = aws_encryption_sdk.StrictAwsKmsMasterKeyProvider(key_ids=[ MASTER_KEY_ARN])
 
     #
-    # The MKP object contains reference to master keys
+    # My secret string
     #
-    mkp = aws_encryption_sdk.KMSMasterKeyProvider(key_ids=[MASTER_KEY_ARN])
-    encryption_context = {'data_type': 'example', 'classification': 'public'}
+    my_plaintext = b'This is some super secret data!'
+    print(' Plain text to encrypt = %s \n ' % my_plaintext)
 
     #
     # Let's encrypt the plaintext data
     #
-    ciphertext, encryptor_header = aws_encryption_sdk.encrypt(
-        source=plaintext, key_provider=mkp, encryption_context=encryption_context
+    print(" let's encrypt ! \n")
+    my_ciphertext, encryptor_header = client.encrypt(
+        source=my_plaintext,
+        key_provider=kms_key_provider
     )
+    print(' My ciphered text  = %s \n ' % my_ciphertext)
 
     #
     # Let's decrypt the ciphertext data
     #
-    decrypted_plaintext, decryptor_header = aws_encryption_sdk.decrypt(
-        source=ciphertext, key_provider=mkp
+    print(" let's decrypt ! \n")
+    decrypted_plaintext, decryptor_header = client.decrypt(
+        source=my_ciphertext,
+        key_provider=kms_key_provider
     )
+    print(' My ciphered text  decrypt = %s \n ' % decrypted_plaintext)
 
-    print(decrypted_plaintext.decode('utf-8'))
 
 def kmsSign():
 
@@ -239,7 +307,7 @@ def kmsSign():
     kms = session.client('kms')
 
     #
-    # Sign a piece of text 
+    # Sign a piece of text
     #
     print('Signing a simple text ')
     response = kms.sign(
@@ -252,12 +320,12 @@ def kmsSign():
     signature = response['Signature']
 
     #
-    # Show signature 
+    # Show signature
     #
     print(f"Signature : {base64.b64encode(signature).decode('ascii')}")
 
     #
-    # verify signature 
+    # verify signature
     #
     response = kms.verify(
         KeyId=SIGNATURE_KEY_ARN,
@@ -272,12 +340,13 @@ def kmsSign():
     else:
         print('Signature is NOT valid')
 
+
 def kmsAsymetricEncrypt():
 
-    # small amount of data only 
+    # small amount of data only
     # see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Client.encrypt
     MESSAGE_TO_CRYPT = b'This is the message to encrypt'
-    ASYNC_KEY_ARN    = 'arn:aws:kms:us-east-1:486652066693:key/5a681bd7-d1f9-44f7-839f-576601255490'
+    ASYNC_KEY_ARN = 'arn:aws:kms:us-east-1:486652066693:key/5a681bd7-d1f9-44f7-839f-576601255490'
 
     #
     # Create a KMS Client object
@@ -294,7 +363,7 @@ def kmsAsymetricEncrypt():
         Plaintext=MESSAGE_TO_CRYPT,
         EncryptionAlgorithm='RSAES_OAEP_SHA_256'
     )
-    print (f"Ciphered text     = {base64.b64encode(ret['CiphertextBlob'])}")
+    print(f"Ciphered text     = {base64.b64encode(ret['CiphertextBlob'])}")
 
     #
     # Decrypt a ciphered text
@@ -305,13 +374,10 @@ def kmsAsymetricEncrypt():
         CiphertextBlob=ret['CiphertextBlob'],
         EncryptionAlgorithm='RSAES_OAEP_SHA_256'
     )
-    print (f"Plaintext message = {ret['Plaintext']}")
+    print(f"Plaintext message = {ret['Plaintext']}")
 
 
 if __name__ == '__main__':
     kmsKeyDemo()
     kmsEncryptionDemo()
-    S3KMSDemo()
     encryptionSDKDemo()
-    kmsSign()
-    kmsAsymetricEncrypt()
